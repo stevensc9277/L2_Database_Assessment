95cb2d8085eb593a271c47a7892eacd929429b96
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-11-08 11:47:55 +1300

    added extra styling for css and search side bar. Also changed colors of boxes (using blue and yellow)

--- site_template/index.php
+++ site_template/index.php
@@ -1,71 +1,18 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-    
-
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="games, apps, ">
-    <meta name="GTT" content="Game / App Database">
-    <meta name="keywords" content="games, apps, ratings">
-    
-    <title>Nobel Prize Winners</title>
-
-    <!-- for multiple fonts change | to %7c * no spaces*  -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
-
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
-
-    
-</head>
-
-<body>
-    
-    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
-        
-    <div class="wrapper">
-    
-        <!-- logo / small image goes here -->
-        <div class="box logo">
-            <a href="index.php"><img src="images/logo.png" width="261" height="150" alt="Trophy" /></a>
-        </div> <!-- / logo -->
-        
-        <div class="box banner">
-            <h1>Nobel Prize Winners</h1>        
-        </div> <!-- / banner -->
-                       
+<?php include("topbit.php") ?>         
             
         <div class="box main">
             <h2>Welcome</h2>
             
-            <p>This database includes information about the Nobel Prize winners since the early 1900s.  </p>     
+            <p>This database includes information about the Nobel Prize winners (laureates) since the early 1900s.  </p>     
             
             <h2>Acknowledgemnts & Sources</h2>
             <ul>
                 <li><a href="https://www.nobelprize.org/prizes/lists/all-nobel-prizes">https://www.nobelprize.org/prizes/lists/all-nobel-prizes</a></li>
                 
-                <li></li>              
+                          
             
             </ul>
             
         </div> <!-- / main -->
         
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
-
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC GTT 20XX
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+<?php include("bottombit.php") ?>
