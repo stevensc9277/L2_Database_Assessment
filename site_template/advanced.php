1f92bc7bc59e0e36515b23e126dd860277307f49
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-11-17 10:49:45 +1300

    recycled code from app database for advanced search. Checks that input matches stuff in database

--- site_template/advanced.php
+++ site_template/advanced.php
@@ -1,4 +1,4 @@
-<?php include("top_bit.php");
+<?php include("topbit.php");
 
     $name = mysqli_real_escape_string($dbconnect, $_POST['name']);
     $recieved = mysqli_real_escape_string($dbconnect, $_POST['recieved']);
@@ -34,34 +34,15 @@
         
     } // end share if / elseif / else
 
- // Age
-    $age_more_less = mysqli_real_escape_string($dbconnect, $_POST['age_more_less']);
-    $age = mysqli_real_escape_string($dbconnect, $_POST['age']);
-    
-    if ($age == "") {
-        $age = 0;
-        $age_more_less = "at least"; // set age to 0 if it is blank
-    }
-
-    if ($age_more_less == "at most") {
-        $age_op = "<=";
-    }
-
-    else {
-        $age_op = ">=";
-        $age = 0;
-    } // end rating if / elseif / else
-
-    $find_sql = "SELECT * FROM `game_details`
-    JOIN genre ON (game_details.GenreID = genre.GenreID)
-    JOIN developer ON (game_details.DeveloperID = developer.developerID)
-    WHERE `Name` LIKE '%$app_name%'
-    AND `DevName` LIKE '%$developer%'
-    AND `Genre` LIKE '%$genre%'
-    AND `Price` $cost_op '$cost'
-    AND (`In App` = $in_app OR `In App` = 0)
-    AND `User Rating` $rate_op '$rating'
-    AND `Age` $age_op ''
+    $find_sql = "SELECT * FROM `laureate_details`
+    JOIN category ON (laureate_details.CategoryID = category.CategoryID)
+    JOIN country ON (laureate_details.CountryID = country.CountryID)
+    WHERE `Firstname` LIKE '%$name%'
+    AND `Country` LIKE '%$country%'
+    AND `Category` LIKE '%$category%'
+    AND `Share` $share_op '$share'
+    AND (`Gender` = $gender OR `Gender` = 0)
+    AND `Year` LIKE '%$recieved%'
     ";
 
     $find_query = mysqli_query($dbconnect, $find_sql);
@@ -74,9 +55,9 @@
             <h2>Advanced Search Results</h2>
             
             <?php 
-            include ("results.php"); 
+            include ("result.php"); 
             ?>
             
             </div> <!-- / main -->
 
-<?php include("bottom_bit.php") ?>
+<?php include("bottombit.php") ?>
